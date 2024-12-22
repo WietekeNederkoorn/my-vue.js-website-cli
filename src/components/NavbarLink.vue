4280a643956463a7b32c28265ea05cb304f6335c
Wieteke <wietekenederkoorn@gmail.com>
2024-12-22 13:21:28 +0100

    Changing NavbarLink

---
+++ src/components/NavbarLink.vue
@@ -0,0 +1,28 @@
+<template>
+    <a 
+        class="nav-link"
+        :class="{active: activePage==index}" 
+        aria-current="page" 
+        :href= "page.link.url"
+        :title="'This link goed to the ${page.link.text} page'"
+    >{{page.link.text}}</a>
+</template>
+
+<script>
+export default{ 
+    props:['page','isActive'],
+    computed:{
+        activeClasses(){
+            return{
+                active: this.isActive,
+                emphasize: this.isActive
+            };
+        }
+    }
+}
+</script>
+<style>
+.emphasize {
+    text-decoration: underline !important;
+}
+</style>
